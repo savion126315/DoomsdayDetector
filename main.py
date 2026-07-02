d49327e221d93e3326bf236a69a05f56b2c7d5dc
Savion Ragster (Woven by Toyota <savion.ragster@woven-planet.global>
2026-07-02 15:30:45 +0200

    Updated syntax for production

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+"""
+Main program for running Doomsday Detector. 
+
+Run with python3 main.py
+
+Savion Ragster and Johnny Tieman 2026
+"""
+
+
 from DoomsdayDef import DoomsdayDetector
 import datetime
 
 
 dd = DoomsdayDetector(["savionragster@gmail.com", "johnnyflips916@gmail.com"])
 
-dd.collection()
-dd.calculator()
-print(dd.percentage_chance)
-dd.reporter()
-
-
-
-# while True:
-   # if datetime.datetime.now().hour == 9 and datetime.datetime.now().minute == 0:
-    # dd.collection()
-   # dd.calculator()
-   # dd.reporter()
+print("Program running, press Ctrl+C to stop.")
+while True:
+   if datetime.datetime.now().hour == 9 and datetime.datetime.now().minute == 0:
+       dd.collection()
+       dd.calculator()
+       dd.reporter()
